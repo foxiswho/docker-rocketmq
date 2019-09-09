72c984ce97aa993415209677d8e11fc3bb4ec58b
fox <foxiswho@gmail.com>
2019-09-09 15:29:33 +0800

    增加备份目录

--- create.j.jenkins.start.sh
+++ create.j.jenkins.start.sh
@@ -4,7 +4,9 @@ DIR=$(cd $(dirname $0); pwd)
 
 
 mkdir -p        ${DIR}/jenkins/home
+mkdir -p        ${DIR}/jenkins/backup
 chmod -R 777    ${DIR}/jenkins/home
+chmod -R 777    ${DIR}/jenkins/backup
 
 kubectl create -f  ${DIR}/jenkins/service.yml
 kubectl create -f  ${DIR}/jenkins/pod.yml
