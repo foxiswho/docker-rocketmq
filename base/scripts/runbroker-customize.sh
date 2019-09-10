83f10e90b46894f532f487c0cd95bbcff3344749
foxwho <foxiswho@gmail.com>
2019-09-10 09:53:44 +0800

    增加 打印参数

--- base/scripts/runbroker-customize.sh
+++ base/scripts/runbroker-customize.sh
@@ -142,6 +142,8 @@ JAVA_OPT="${JAVA_OPT} -Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${BASE_DIR}/lib"
 #JAVA_OPT="${JAVA_OPT} -Xdebug -Xrunjdwp:transport=dt_socket,address=9555,server=y,suspend=n"
 JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
 JAVA_OPT="${JAVA_OPT} -cp ${CLASSPATH}"
+echo "======================================"
+echo $JAVA_OPT
 
 numactl --interleave=all pwd > /dev/null 2>&1
 if [ $? -eq 0 ]
