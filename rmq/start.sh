c9d56952da36c9b94883dc46ad5eab04fe81498e
fox <80005368@qq.com>
2020-02-25 14:54:11 +0800

    增加说明

--- rmq/start.sh
+++ rmq/start.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-
+# 创建目录
 mkdir -p ./rmqs/logs
 mkdir -p ./rmqs/store
 mkdir -p ./rmq/logs
 mkdir -p ./rmq/store
 
+# 设置目录权限
 chmod -R 777 ./rmqs/logs
 chmod -R 777 ./rmqs/store
 chmod -R 777 ./rmq/logs
 chmod -R 777 ./rmq/store
 
+# 下载并启动容器，且为 后台 自动启动
 docker-compose up -d
+
+
+# 显示 rocketmq 容器
+docker ps |grep rocketmq
